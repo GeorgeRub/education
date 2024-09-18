a138591cc7377e99f087e9191d8ca44739223cd1
Heorhii Rublov <rublev.georgij@gmail.com>
2024-09-18 05:15:55 -0300

    Update Customer.java

--- spring/section 6/validation/src/main/java/com/mvc/validation/model/Customer.java
+++ spring/section 6/validation/src/main/java/com/mvc/validation/model/Customer.java
@@ -1,9 +1,6 @@
 package com.mvc.validation.model;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 @Data
@@ -18,5 +15,8 @@ public class Customer {
     @Min(value=0, message="must be greater than or equal to zero")
     @Max(value=10, message="must be less than or equal to 10")
     private int freePasses;
+
+    @Pattern(regexp = "^[a-zA-z0-9]{5}", message = "Only 5 characters/digits")
+    private String postalCode;
 
 }
