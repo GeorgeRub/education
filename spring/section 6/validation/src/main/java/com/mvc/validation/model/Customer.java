53a1c5c3dd78376544699748c5aa3b2edf4c5ab1
Heorhii Rublov <rublev.georgij@gmail.com>
2024-09-18 05:03:12 -0300

    spring validation of numbers

--- spring/section 6/validation/src/main/java/com/mvc/validation/model/Customer.java
+++ spring/section 6/validation/src/main/java/com/mvc/validation/model/Customer.java
@@ -1,5 +1,7 @@
 package com.mvc.validation.model;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
@@ -12,5 +14,9 @@ public class Customer {
     @NotNull(message = "Should not be empty")
     @Size(min=1, message = "Should be more than 1 character")
     private String lastName;
+
+    @Min(value=0, message="must be greater than or equal to zero")
+    @Max(value=10, message="must be less than or equal to 10")
+    private int freePasses;
 
 }
