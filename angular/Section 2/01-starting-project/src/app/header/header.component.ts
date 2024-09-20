729d1185dae56a39b45af56d023f15a5d17b4d75
Heorhii Rublov <rublev.georgij@gmail.com>
2024-09-20 10:35:59 -0300

    angular

---
+++ angular/Section 2/01-starting-project/src/app/header/header.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  styleUrls: ['./header.component.css'],
+  templateUrl: './header.component.html',
+})
+export class HeaderComponent {
+
+}
