1cc0434d4ba20d295cfeb447566b989b1f5bf64f
Heorhii Rublov <rublev.georgij@gmail.com>
2024-09-22 21:23:06 -0300

    angular

--- angular/Section 2/01-starting-project/src/app/user/user.component.ts
+++ angular/Section 2/01-starting-project/src/app/user/user.component.ts
@@ -1,11 +1,6 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {NgOptimizedImage} from "@angular/common";
-
-interface User {
-  id: string;
-  avatar: string;
-  name: string;
-}
+import {type User} from "./user.model";
 
 @Component({
   selector: 'app-user',
