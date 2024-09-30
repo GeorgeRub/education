307ab9f56cbd42b54bd5077288db6fe3a7353c18
Heorhii Rublov <rublev.georgij@gmail.com>
2024-09-30 10:56:37 -0300

    Spring finished

--- spring/section 10/aop-before/src/main/java/com/mvc/aopbefore/AopBeforeApplication.java
+++ spring/section 10/aop-before/src/main/java/com/mvc/aopbefore/AopBeforeApplication.java
@@ -1,6 +1,7 @@
 package com.mvc.aopbefore;
 
 import com.mvc.aopbefore.dao.AccountDAO;
+import com.mvc.aopbefore.models.Account;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -22,7 +23,11 @@ public class AopBeforeApplication {
     }
 
     private void demoTheBeforeAdvice(AccountDAO accountDAO) {
-        accountDAO.addAccount();
+        Account account = new Account();
+        account.setFirstName("John");
+        account.setLastName("Doe");
+        accountDAO.addAccount(account);
+        System.out.println("Account after added " + account);
     }
 
 }
--- spring/section 10/aop-before/src/main/java/com/mvc/aopbefore/dao/AccountDAOImpl.java
+++ spring/section 10/aop-before/src/main/java/com/mvc/aopbefore/dao/AccountDAOImpl.java
@@ -1,11 +1,13 @@
 package com.mvc.aopbefore.dao;
 
+import com.mvc.aopbefore.models.Account;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class AccountDAOImpl implements AccountDAO {
     @Override
-    public void addAccount() {
+    public void addAccount(Account account) {
         System.out.println(getClass().getName() + " --->>> Doing some work!!!");
+        System.out.println("Account: " + account);
     }
 }
