daf578af27b2a3ef4f98d680f9b06b7ef4058720
Rublov, Heorhii <george.rublov@cgi.com>
2024-10-15 11:42:08 -0300

    angular

--- angular/Section 6/01-starting-project/src/app/shared/button/button.component.ts
+++ angular/Section 6/01-starting-project/src/app/shared/button/button.component.ts
@@ -1,15 +1,12 @@
 import {Component, input} from '@angular/core';
 
 @Component({
-  selector: 'app-button',
+  selector: 'button[app-button]',
   standalone: true,
   imports: [],
   templateUrl: './button.component.html',
   styleUrl: './button.component.css'
 })
 export class ButtonComponent {
-
-  value = input.required<string>()
-  icon = input.required<string>()
 
 }
