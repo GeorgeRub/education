23715676133e08c4710b0b478ec21e91390c2774
Heorhii Rublov <rublev.georgij@gmail.com>
2024-10-16 05:31:40 -0300

    angular

--- angular/Section 6/01-starting-project/src/app/dashboard/server-status/server-status.component.ts
+++ angular/Section 6/01-starting-project/src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {DashboardItemComponent} from "../dashboard-item/dashboard-item.component";
 
 @Component({
@@ -10,6 +10,25 @@ import {DashboardItemComponent} from "../dashboard-item/dashboard-item.component
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css'
 })
-export class ServerStatusComponent {
-  currentStatus = 'online';
+export class ServerStatusComponent implements OnInit {
+  currentStatus: 'online' | 'offline' | 'unknown' = 'online';
+
+  constructor() {
+    console.log('Server Status Component loaded on constructor');
+  }
+
+  ngOnInit(){
+    console.log('Server Status Component loaded on initialized.');
+    setInterval(() => {
+      const rnd = Math.random();
+      if(rnd < 0.5){
+        this.currentStatus = 'online';
+      }else if (rnd < 0.9){
+        this.currentStatus = 'offline';
+      }else{
+        this.currentStatus = 'unknown';
+      }
+    },5000)
+  }
+
 }
