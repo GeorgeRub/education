9784399a24021099d25fadf531eeccf2517ad190
Heorhii Rublov <rublev.georgij@gmail.com>
2024-10-22 05:59:35 -0300

    angular pipe

--- angular/Section 8/pipe-starting-project/src/app/temperature.pipe.ts
+++ angular/Section 8/pipe-starting-project/src/app/temperature.pipe.ts
@@ -31,7 +31,7 @@ export class TemperaturePipe implements PipeTransform {
             symbol = outputType === 'cel' ? 'C' : 'F';
         }
 
-        return `${outputTemp} ${symbol}`;
+        return `${outputTemp.toFixed(2)} ${symbol}`;
     }
 
 }
