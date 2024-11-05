12efbaf52e7263aa8a7548524995a27928a2db4b
Rublov, Heorhii <george.rublov@cgi.com>
2024-11-05 09:21:58 -0400

    Angular Http

--- angular/Section 12/http-starting-project/src/app/places/user-places/user-places.component.ts
+++ angular/Section 12/http-starting-project/src/app/places/user-places/user-places.component.ts
@@ -1,7 +1,7 @@
 import {Component, DestroyRef, inject, OnInit, signal} from '@angular/core';
 
-import { PlacesContainerComponent } from '../places-container/places-container.component';
-import { PlacesComponent } from '../places.component';
+import {PlacesContainerComponent} from '../places-container/places-container.component';
+import {PlacesComponent} from '../places.component';
 import {Place} from "../place.model";
 import {PlacesService} from "../places.service";
 
@@ -40,5 +40,12 @@ export class UserPlacesComponent implements OnInit {
       request.unsubscribe();
     })
 
+  }
+
+  onRemovePlace(event: Place) {
+    const request = this.placesService.removeUserPlace(event).subscribe();
+    this.destroyRef.onDestroy(() => {
+      request.unsubscribe();
+    })
   }
 }
--- angular/Section 12/http-starting-project/src/main.ts
+++ angular/Section 12/http-starting-project/src/main.ts
@@ -1,9 +1,40 @@
 import {bootstrapApplication} from '@angular/platform-browser';
 
 import {AppComponent} from './app/app.component';
-import {provideHttpClient} from "@angular/common/http";
+import {HttpEventType, HttpHandlerFn, HttpRequest, provideHttpClient, withInterceptors} from "@angular/common/http";
+import {tap} from "rxjs";
 
+function loggingInterceptor(request: HttpRequest<unknown>, next: HttpHandlerFn) {
+  // const req = request.clone({
+  //   headers:request.headers.set('X-DEBUG','TESTING')
+  // });
+  console.log('Interceptor')
+  console.log(request);
+  return next(request).pipe(
+    tap({
+      next: event => {
+        switch (event.type) {
+          case HttpEventType.Response :{
+            console.log(">>>--- TYPE Response ---<<<")
+            console.log(event);
+            console.log(event.status);
+            console.log(event.body);
+            break
+          }
+          case HttpEventType.ResponseHeader: {
+            console.log(">>>--- TYPE ResponseHeader ---<<<")
+            console.log(event);
+            console.log(event.status);
+            console.log(event);
+          }
+        }
+      }
+    })
+  );
+}
 
 bootstrapApplication(AppComponent, {
-  providers: [provideHttpClient()]
+  providers: [provideHttpClient(
+    withInterceptors([loggingInterceptor])
+  )]
 }).catch((err) => console.error(err));
