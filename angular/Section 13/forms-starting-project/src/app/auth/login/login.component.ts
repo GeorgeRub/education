3953f3042eb8260a3023fa1d854afdab1180fd47
Rublov, Heorhii <george.rublov@cgi.com>
2024-11-05 15:55:03 -0400

    Angular preparing the forms to reactive

--- angular/Section 13/forms-starting-project/src/app/auth/login/login.component.ts
+++ angular/Section 13/forms-starting-project/src/app/auth/login/login.component.ts
@@ -1,6 +1,5 @@
-import {afterNextRender, Component, DestroyRef, inject, viewChild} from '@angular/core';
-import {FormsModule, NgForm} from "@angular/forms";
-import {debounceTime} from "rxjs";
+import {Component} from '@angular/core';
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
@@ -12,54 +11,5 @@ import {debounceTime} from "rxjs";
   ]
 })
 export class LoginComponent {
-  private form = viewChild.required<NgForm>('form')
-  private destryRef = inject(DestroyRef)
 
-  constructor() {
-    afterNextRender(() => {
-
-      const savedForm = window.localStorage.getItem('saved-login-form')
-      if (savedForm) {
-        const loadedForm = JSON.parse(savedForm)
-        const savedEmail = loadedForm.email
-        setTimeout(
-          ()=> {
-            this.form().controls['email'].setValue(savedEmail)
-          },1
-        )
-
-        // this.form().setValue({
-        //   email: savedEmail,
-        //   password: ''
-        // })
-      }
-
-      const subscription = this.form().valueChanges?.pipe(
-        debounceTime(800)
-      ).subscribe({
-        next: (value) => {
-          console.log(value)
-          window.localStorage.setItem(
-            'saved-login-form',
-            JSON.stringify({email: value.email, password: value.password}))
-        }
-      })
-      this.destryRef.onDestroy(() => {
-        subscription?.unsubscribe()
-      })
-    })
-  }
-
-//   isEmailValid(): boolean{
-// form
-//   }
-
-  onSubmit(formData: NgForm) {
-    if (formData.form.invalid) return
-    const email = formData.form.value.email
-    const password = formData.form.value.password
-    console.log(email, 'and', password)
-    formData.form.reset()
-    // formData.form.disable()
-  }
 }
