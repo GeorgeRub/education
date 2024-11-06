f9bece43079d3901cd4ff36e4c257c82713b592b
Rublov, Heorhii <george.rublov@cgi.com>
2024-11-06 16:06:15 -0400

    angular forms

--- angular/Section 13/forms-starting-project/src/app/auth/login/login.component.ts
+++ angular/Section 13/forms-starting-project/src/app/auth/login/login.component.ts
@@ -1,5 +1,32 @@
-import {Component} from '@angular/core';
-import {FormsModule} from "@angular/forms";
+import {Component, DestroyRef, inject, OnInit} from '@angular/core';
+import {AbstractControl, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
+import {debounceTime, of} from "rxjs";
+
+function mustContentQuestionMark(control: AbstractControl) {
+  console.log('custom validating')
+  if (control.value.includes('?')) {
+    return null
+  }
+  return {doesNotContainQuestionMark: true}
+
+}
+
+function emailIsUnique(control: AbstractControl) {
+  if (control.value !== 'test@example.com') {
+    console.log('return null')
+    return of(null)
+  }
+  console.log('return NOT null')
+  return of({notUnique: true})
+}
+
+//This works only on NOT server side
+let initialedEmail = '';
+const savedForm = window.localStorage.getItem('saved-login-form')
+if (savedForm) {
+  const loadedForm = JSON.parse(savedForm)
+  initialedEmail = loadedForm.email
+}
 
 @Component({
   selector: 'app-login',
@@ -7,9 +34,63 @@ import {FormsModule} from "@angular/forms";
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
   imports: [
-    FormsModule
+    FormsModule, ReactiveFormsModule
   ]
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
+  destroyRef = inject(DestroyRef)
+
+  inputForm = new FormGroup({
+    email: new FormControl(initialedEmail, {
+      validators: [Validators.required, Validators.email],
+      asyncValidators: [emailIsUnique]
+    }),
+    password: new FormControl('', {
+      validators: [Validators.required, Validators.minLength(6), mustContentQuestionMark]
+    }),
+  })
+
+  get emailIsInvalid() {
+    return this.inputForm.controls.email.invalid
+      && this.inputForm.controls.email.touched
+      && this.inputForm.controls.email.dirty;
+  }
+
+  get passwordIsInvalid() {
+    return this.inputForm.controls.password.invalid
+      && this.inputForm.controls.password.touched
+      && this.inputForm.controls.password.dirty;
+  }
+
+  ngOnInit() {
+
+    //This will work on a server side
+    //
+    // const savedForm = window.localStorage.getItem('saved-login-form')
+    // if (savedForm) {
+    //   const loadedForm = JSON.parse(savedForm)
+    //   // this.inputForm.controls.email.setValue(loadedForm.email)
+    //   this.inputForm.patchValue({
+    //     email: loadedForm.email
+    //   })
+    // }
+
+
+    const valChange = this.inputForm.valueChanges
+      .pipe(debounceTime(800))
+      .subscribe({
+        next: value => {
+          window.localStorage.setItem('saved-login-form', JSON.stringify({email: value.email}))
+        }
+      })
+    this.destroyRef.onDestroy(() => {
+      valChange.unsubscribe()
+    })
+  }
+
+  onSubmit() {
+    console.log(this.inputForm)
+  }
 
 }
--- angular/Section 13/forms-starting-project/src/app/auth/signup/signup.component.ts
+++ angular/Section 13/forms-starting-project/src/app/auth/signup/signup.component.ts
@@ -1,9 +1,59 @@
-import { Component } from '@angular/core';
+import {Component, DestroyRef, inject, OnInit} from '@angular/core';
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
+import {debounceTime} from "rxjs";
 
 @Component({
   selector: 'app-signup',
   standalone: true,
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css',
+  imports: [
+    ReactiveFormsModule, FormsModule
+  ]
 })
-export class SignupComponent {}
+export class SignupComponent implements OnInit {
+
+  destroyRef = inject(DestroyRef)
+
+  welcomeForm = new FormGroup({
+    email: new FormControl('', {
+      validators: [Validators.required, Validators.email]
+    }),
+    password: new FormControl('', {
+      validators: [Validators.required, Validators.minLength(6)]
+    }),
+    confirmPassword: new FormControl('')
+  })
+
+  ngOnInit() {
+
+    const savedWelcomeForm = window.localStorage.getItem('welcome-form')
+    if (savedWelcomeForm) {
+      const loadedWelcomeForm = JSON.parse(savedWelcomeForm)
+      this.welcomeForm.patchValue({
+        email: loadedWelcomeForm.email
+      })
+    }
+
+    const valChanged = this.welcomeForm
+      .valueChanges
+      .pipe(debounceTime(800)).subscribe({
+        next: value => {
+          window.localStorage.setItem('welcome-form', JSON.stringify({email: value.email}))
+        }
+      })
+
+    this.destroyRef.onDestroy(() => {
+      valChanged.unsubscribe()
+    })
+  }
+
+  onSubmit() {
+    // console.log(this.welcomeForm.value)
+  }
+
+  onReset() {
+    this.welcomeForm.reset()
+  }
+
+}
