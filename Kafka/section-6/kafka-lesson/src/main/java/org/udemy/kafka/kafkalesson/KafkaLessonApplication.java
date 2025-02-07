7c3dbcd0fda94b091f52dda865164e8cc0cf7262
Heorhii Rublov <rublev.georgij@gmail.com>
2025-02-07 10:08:43 -0400

    kafka

---
+++ Kafka/section-6/kafka-lesson/src/main/java/org/udemy/kafka/kafkalesson/KafkaLessonApplication.java
@@ -0,0 +1,13 @@
+package org.udemy.kafka.kafkalesson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaLessonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaLessonApplication.class, args);
+    }
+
+}
---
+++ Kafka/section-6/kafka-lesson/src/test/java/org/udemy/kafka/kafkalesson/KafkaLessonApplicationTests.java
@@ -0,0 +1,13 @@
+package org.udemy.kafka.kafkalesson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaLessonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
